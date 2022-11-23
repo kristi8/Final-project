5a61959345f868d10870f1e2760788c805e069bb
kristi8 <qristina.chelidze@gmail.com>
2022-11-23 21:55:13 +0400

    section 3-4, footer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 const header = document.querySelector("header");
 
@@ -6,3 +6,7 @@ window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 
 });
+
+// let menu = document.querySelector('#burger-bar');
+// let navbar = document.querySelectorAll('.nav-ul');
+
